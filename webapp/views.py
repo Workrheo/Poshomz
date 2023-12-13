58e33141deb2e4eed4ff35f515307c76f693294e
Srushti41 <srushti@workrheo.in>
2023-12-13 14:57:10 +0530

    first committe

--- webapp/views.py
+++ webapp/views.py
@@ -35,13 +35,14 @@ def home(request):
 
 def about(request):
     
-    
+    post_list = Post.objects.all()
     team_list = Team.objects.all()
     testimonials = Testimonials.objects.all()
+    product_list = PRODUCT_CATEGORY.objects.all()
 
 
-    context = {'meta_title': 'About Combine Design | Transforming Spaces',
-                'team_list':team_list, 'testimonials' : testimonials ,
+    context = {'meta_title': 'About Combine Design | Transforming Spaces', 'product_list':product_list,
+                'post_list' : post_list ,'team_list':team_list, 'testimonials' : testimonials ,
                'meta_description': 'Transforming spaces with personalized designs in Bengaluru. Discover inspired living with Combine Design.'
                }
 
@@ -52,13 +53,16 @@ def about(request):
 
 def our_work(request):
     work_list = Our_WORKS_CATEGORY.objects.all()
+    post_list = Post.objects.all()
+    testimonials = Testimonials.objects.all()
+   
    
 
     
     
     context = {
 
-		'work_list': work_list ,
+		'work_list': work_list ,'post_list' : post_list ,'testimonials': testimonials ,
         'meta_title': '100+ Designs for Your Interiors | 2&3 BHK Interiors ',
         'meta_description': 'Explore our portfolio, which showcases exceptional interior designs crafted by Combine Design. Let our creations inspire your imagination.'
 
@@ -72,10 +76,12 @@ def our_work(request):
 def our_work_detail(request, id):
     our_work_detail = Our_WORKS_CATEGORY.objects.get(id=id)
     our_work_images = WorkItem.objects.filter(category=our_work_detail)
+    post_list = Post.objects.all()
+    testimonials = Testimonials.objects.all()
     
     
     context = {
-        'our_work_detail': our_work_detail , 'our_work_images':our_work_images ,
+        'our_work_detail': our_work_detail , 'our_work_images':our_work_images ,'post_list' : post_list ,'testimonials': testimonials ,
         'meta_title':our_work_detail.meta_title, 
         'meta_description': our_work_detail.meta_description
     }
@@ -104,9 +110,11 @@ def product(request):
 def product_detail(request , id):
     product_detail = PRODUCT_CATEGORY.objects.get(id=id)
     product_list = Product.objects.filter(category=product_detail)
+    testimonials = Testimonials.objects.all()
+    
 
     context = {
-        'product_detail': product_detail , 'product_list':product_list ,
+        'product_detail': product_detail , 'product_list':product_list ,'testimonials': testimonials ,
         'meta_title':product_detail.meta_title, 
         'meta_description': product_detail.meta_description,
 
@@ -152,11 +160,12 @@ def style_detail(request , id):
 
 def post_list(request):
     post_list = Post.objects.all()
+    testimonials = Testimonials.objects.all()
 
 
 
     context = {
-        'post_list': post_list,
+        'post_list': post_list,'testimonials': testimonials ,
  'meta_title': 'Stay Updated - Tips from Combine Design’s Experts',
    'meta_description': 'Welcome to Combine Design Bengaluru. Explore our innovative interior design solutions.'
     }
@@ -167,33 +176,35 @@ def post_list(request):
 
 
 def post_detail(request, id):
-	post_detail = Post.objects.get(id=id)
-	categories = Category.objects.all()
-	comments = Comment.objects.filter(post=post_detail)
+    testimonials = Testimonials.objects.all()
+    post_detail = Post.objects.get(id=id)
+    categories = Category.objects.all()
+    comments = Comment.objects.filter(post=post_detail)
+    
 
-	if request.method =='POST' :
-		comment_form = CommentForm(request.POST)
-		if comment_form.is_valid():
-			new_comment = comment_form.save(commit=False)
-			new_comment.user = request.user
-			new_comment.post = post_detail
-			new_comment.save()
+    if request.method =='POST' :
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = post_detail
+            new_comment.save()
 
-	else:
-		comment_form = CommentForm()
-
-
+    else:
+        comment_form = CommentForm()
 
 
 
-	context = {
-		'post_detail':post_detail , 'categories' : categories , 'comments': comments , 
+
+
+    context = {
+		'post_detail':post_detail , 'testimonials': testimonials ,'categories' : categories , 'comments': comments , 
 'comment_form':comment_form ,  'meta_title': 'Combine Design Bengaluru | Home',
    'meta_description': 'Welcome to Combine Design Bengaluru. Explore our innovative interior design solutions.'
 
 	}
 
-	return render(request , 'webapp/post_detail.html', context)
+    return render(request , 'webapp/post_detail.html', context)
 
 def contact(request):
 
